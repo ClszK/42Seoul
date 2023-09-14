ec38bdfb1e2cee628fb868cbbd420279a8653b39
Jehoon Lee <jeholee@cx1r4s8.42seoul.kr>
2023-09-15 04:42:05 +0900

    so_long start

---
+++ so_long/includes/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeholee <jeholee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 03:11:25 by jeholee           #+#    #+#             */
+/*   Updated: 2023/09/15 04:38:04 by jeholee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
+
+// open, close, read, write, malloc, free, perror, strerror, exit
---
+++ so_long/test.c
@@ -0,0 +1,7 @@
+#include "./ft_printf/includes/ft_printf.h"
+
+int main()
+{
+	ft_printf("hi");
+	return 0;
+}
