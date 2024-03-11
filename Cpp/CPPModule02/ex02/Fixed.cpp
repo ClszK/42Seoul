c4368001fdb98d70710b558628e355e793a7a9e2
Jehoon Lee <jeholee@c3r3s2.42seoul.kr>
2024-03-11 23:46:44 +0900

    cpp:module02 ex02

---
+++ Cpp/CPPModule02/ex02/Fixed.cpp
@@ -0,0 +1,144 @@
+#include "Fixed.hpp"
+
+const int Fixed::num_of_fractional_bit_ = 8;
+
+Fixed::Fixed() : fixed_point_num_(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int convert_fixed_point_val)
+    : fixed_point_num_(convert_fixed_point_val
+                       << Fixed::num_of_fractional_bit_) {
+  std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float convert_fixed_point_val_f) {
+  std::cout << "Float constructor called" << std::endl;
+  fixed_point_num_ = static_cast<int>(
+      roundf(convert_fixed_point_val_f * (1 << Fixed::num_of_fractional_bit_)));
+}
+
+Fixed::Fixed(const Fixed& other) {
+  std::cout << "Copy constructor called" << std::endl;
+  *this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+  if (this != &other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    fixed_point_num_ = other.fixed_point_num_;
+  }
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fix) {
+  os << fix.toFloat();
+  return os;
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return fixed_point_num_;
+}
+
+void Fixed::setRawBits(int const raw) {
+  std::cout << "setRawBits member function called" << std::endl;
+  fixed_point_num_ = raw;
+}
+
+float Fixed::toFloat(void) const {
+  return (static_cast<float>(fixed_point_num_) /
+          (1 << Fixed::num_of_fractional_bit_));
+}
+
+int Fixed::toInt(void) const {
+  return (fixed_point_num_ >> Fixed::num_of_fractional_bit_);
+}
+
+bool Fixed::operator>(const Fixed& ref) const {
+  return (fixed_point_num_ > ref.fixed_point_num_);
+}
+
+bool Fixed::operator<(const Fixed& ref) const {
+  return (fixed_point_num_ < ref.fixed_point_num_);
+}
+
+bool Fixed::operator<=(const Fixed& ref) const {
+  return (fixed_point_num_ <= ref.fixed_point_num_);
+}
+
+bool Fixed::operator>=(const Fixed& ref) const {
+  return (fixed_point_num_ >= ref.fixed_point_num_);
+}
+
+bool Fixed::operator==(const Fixed& ref) const {
+  return (fixed_point_num_ == ref.fixed_point_num_);
+}
+
+bool Fixed::operator!=(const Fixed& ref) const {
+  return (fixed_point_num_ != ref.fixed_point_num_);
+}
+
+Fixed Fixed::operator+(const Fixed& ref) const {
+  Fixed temp(fixed_point_num_ + ref.fixed_point_num_);
+  return temp;
+}
+
+Fixed Fixed::operator-(const Fixed& ref) const {
+  Fixed temp(fixed_point_num_ - ref.fixed_point_num_);
+  return temp;
+}
+
+Fixed Fixed::operator*(const Fixed& ref) const {
+  Fixed temp(this->toFloat() * ref.toFloat());
+  return temp;
+}
+
+Fixed Fixed::operator/(const Fixed& ref) const {
+  Fixed temp(this->toFloat() / ref.toFloat());
+  return temp;
+}
+
+Fixed& Fixed::operator++() {
+  ++fixed_point_num_;
+  return *this;
+}
+
+Fixed& Fixed::operator--() {
+  --fixed_point_num_;
+  return *this;
+}
+
+const Fixed Fixed::operator++(int) {
+  Fixed temp = *this;
+  ++fixed_point_num_;
+  return temp;
+}
+
+const Fixed Fixed::operator--(int) {
+  Fixed temp = *this;
+  --fixed_point_num_;
+  return temp;
+}
+
+Fixed& Fixed::min(Fixed& fixed_1, Fixed& fixed_2) {
+  if (fixed_1 < fixed_2) return fixed_1;
+  return fixed_2;
+}
+
+const Fixed& Fixed::min(const Fixed& fixed_1, const Fixed& fixed_2) {
+  if (fixed_1 < fixed_2) return fixed_1;
+  return fixed_2;
+}
+
+Fixed& Fixed::max(Fixed& fixed_1, Fixed& fixed_2) {
+  if (fixed_1 > fixed_2) return fixed_1;
+  return fixed_2;
+}
+
+const Fixed& Fixed::max(const Fixed& fixed_1, const Fixed& fixed_2) {
+  if (fixed_1 > fixed_2) return fixed_1;
+  return fixed_2;
+}
---
+++ Cpp/CPPModule02/ex02/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "Fixed.hpp"
+
+int main(void) {
+  Fixed a;
+  Fixed const b(Fixed(5.05f) * Fixed(2));
+
+  std::cout << a << std::endl;
+  std::cout << ++a << std::endl;
+  std::cout << a << std::endl;
+  std::cout << a++ << std::endl;
+  std::cout << a << std::endl;
+  std::cout << b << std::endl;
+  std::cout << Fixed::max(a, b) << std::endl;
+
+  std::cout << std::endl << "-----------------------" << std::endl << std::endl;
+
+  Fixed aa(2);
+  Fixed bb(2.1f);
+
+  std::cout << "a is " << a << std::endl;
+  std::cout << "b is " << b << std::endl << std::endl;
+
+  std::cout << "< is " << ((a < b) ? "true" : "false") << std::endl;
+  std::cout << "> is " << ((a > b) ? "true" : "false") << std::endl;
+  std::cout << "<= is " << ((a <= b) ? "true" : "false") << std::endl;
+  std::cout << ">= is " << ((a >= b) ? "true" : "false") << std::endl;
+  std::cout << "== is " << ((a == b) ? "true" : "false") << std::endl;
+  std::cout << "!= is " << ((a != b) ? "true" : "false") << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "aa * bb is " << aa * bb << std::endl;
+  std::cout << "aa / bb is " << aa / bb << std::endl;
+  std::cout << "aa + bb is " << aa + bb << std::endl;
+  std::cout << "aa - bb is " << aa - bb << std::endl;
+  return 0;
+}
