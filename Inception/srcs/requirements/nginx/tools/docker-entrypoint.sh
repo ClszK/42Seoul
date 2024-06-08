a4061961390ba43344ad590250088a7008c48364
clszK <dlwpgns0@gmail.com>
2024-06-08 06:04:25 +0000

    Inception: replace default.conf.template to default.conf

--- Inception/srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ Inception/srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -6,6 +6,4 @@ envsubst '\$DOMAIN_NAME \$SSL_KEY_PATH \$SSL_CERT_PATH' \
 			< ${CONF_PATH}/default.conf.template \
 			> ${CONF_PATH}/default.conf
 
-rm -f ${CONF_PATH}/default.conf.template /docker-entrypoint.sh
-
 exec "$@"
