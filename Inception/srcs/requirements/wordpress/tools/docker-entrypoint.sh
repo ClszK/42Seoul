515e4fe896d02d76d5915b11503e393a72f1f22d
root <root@Inception.myguest.virtualbox.org>
2024-06-16 22:46:58 +0900

    Inception: Add wordpress user

--- Inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ Inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -42,8 +42,16 @@ if ! wp core is-installed --allow-root; then
     --admin_user="$WORDPRESS_ADMIN_USER" \
     --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
     --admin_email="$WORDPRESS_ADMIN_EMAIL" \
-    --skip-email --allow-root
+    --skip-email \
+	--allow-root
+
+  wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
+	--user_pass=$WORDPRESS_USER_PASSWORD \
+	--role=author \
+	--allow-root
 fi
+
+
 
 echo "Start php-fpm82"
 # Keep the container running
